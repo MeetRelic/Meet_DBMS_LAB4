ca823601785bc7583d579251649a56964c69fadc
MeetRelic <meetngandhi@gmail.com>
2023-09-10 15:20:22 +0530

    updating procedure

--- test_dbms_lab_4.sql
+++ test_dbms_lab_4.sql
@@ -195,5 +195,61 @@ WHERE
 -- Attached stored procedured sql file seperately 
 -- Q9 . ) Create a stored procedure to display supplier id, name, rating and Type_of_supplier. If rating >4 then “Genuine Supplier” if rating >2 “Average Supplier” else “Supplier should not be considered”.
 call proc();
--- Delimiter is ; which we have used in our query we can customly define Delimeter and use it . 
+-- Delimiter is ; which we have used in our query we can customly define Delimeter and use it 
+-- solution is atatched below and also in seperate storeproc.sql file 
+
+-- 
+-- BEGIN
+--     SELECT
+--         report.supp_id,
+--         report.supp_name,
+--         report.Average,
+--         CASE
+--             WHEN report.Average = 5 THEN 'Excellent Service'
+--             WHEN report.Average > 4 THEN 'Good Service'
+--             WHEN report.Average > 2 THEN 'Average Service'
+--             ELSE 'Poor Service'
+--         END AS Type_of_Service
+--     FROM
+--         (
+--             SELECT
+--                 final.supp_id,
+--                 supplier.supp_name,
+--                 final.Average
+--             FROM
+--                 (
+--                     SELECT
+--                         test2.supp_id,
+--                         SUM(test2.rat_ratstars) / COUNT(test2.rat_ratstars) AS Average
+--                     FROM
+--                         (
+--                             SELECT
+--                                 supplier_pricing.supp_id,
+--                                 test.ORD_ID,
+--                                 test.RAT_RATSTARS
+--                             FROM
+--                                 supplier_pricing
+--                             INNER JOIN
+--                                 (
+--                                     SELECT
+--                                         `order`.pricing_id,
+--                                         rating.ORD_ID,
+--                                         rating.RAT_RATSTARS
+--                                     FROM
+--                                         `order`
+--                                     INNER JOIN
+--                                         rating ON rating.`ord_id` = `order`.ord_id
+--                                 ) AS test
+--                                 ON test.pricing_id = supplier_pricing.pricing_id
+--                         ) AS test2
+--                     GROUP BY
+--                         supplier_pricing.supp_id
+--                 ) AS final
+--             INNER JOIN
+--                 supplier
+--             WHERE
+--                 final.supp_id = supplier.supp_id
+--         ) AS report;
+-- END
+
 
